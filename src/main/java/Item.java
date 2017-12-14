d9e7e58bdbc020a07c26e42b03f02555144a8e7f
dbunin <bunind@live.com>
2017-12-14 20:22:35 +0100

    -added Item class

---
+++ src/main/java/Item.java
@@ -0,0 +1,11 @@
+public class Item {
+    private String genre;
+    private String format;
+    private String year;
+
+    public Item(String genre, String format, String year){
+        this.genre = genre;
+        this.format = format;
+        this.year = year;
+    }
+}
